5572b030854be2b70d896e672766214330dc0c4c
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-10 18:22:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+    programmer_hash{:grace_hopper}
+
 end
 
 def alan_kay_is_known_for
@@ -36,6 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
