b6b850cc7008727ebe876067d59492805b85b196
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-10 18:32:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -140,6 +140,7 @@ def adding_to_dennis
       }
     }
 
-    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 
 end
